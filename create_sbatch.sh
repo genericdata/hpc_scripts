9111d8534efb24b9503930d88b13a434c217ada6
Eric Borenstein <eb167@nyu.edu>
2025-01-09 12:42:35 -0500

    updated create_sbatch

--- create_sbatch.sh
+++ create_sbatch.sh
@@ -1,47 +1,35 @@
 #!/bin/bash
 
-# Initialize our own variables:
-output_file=""
-gpu=0
+output=$1
 
-while getopts "h?vg" opt; do
-  case "$opt" in
-    h|\?)
-      echo "Usage: $0 [-g] filename" 
-      echo "Create a basic SBATCH template"
-      echo "    -g              GPU - add line to request GPUs"
-      exit 0
-      ;;
-    g)  gpu=1
-      ;;
-  esac
-done
-
-shift $((OPTIND-1))
-output=${@}.sbatch
+if [ -f ./${output}.sbatch ]; then
+    echo "File \"${output}.sbatch\" already exists"
+    exit 1
+fi
 
 echo """#!/bin/bash -e
 
-# SBATCH OPTIONS (https://slurm.schedmd.com/sbatch.html)
-#SBATCH --job-name=${1}
-#SBATCH --mail-type=END,FAIL     # Mail events (NONE, BEGIN, END, FAIL, ALL)
+# More Information: https://slurm.schedmd.com/sbatch.html
+#SBATCH --job-name=${output}
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=1G
-#SBATCH --time=00-01:00:00       # Time limit days-hours:minutes:seconds
-#SBATCH --output=slurm_%j.log""" > ./$output
+#SBATCH --output=slurm_${output}_%j.log
+#SBATCH --time=00-01:00:00       # Run time (dd-hh:mm:ss)
+#SBATCH --mail-type=END,FAIL     # Mail events: NONE, BEGIN, END, FAIL, ALL
+# #SBATCH --account=users        # Uncomment and change if necessary
 
+# [GPU]
+# #SBATCH --gres=gpu:1
+# #SBATCH --constraint=rtx8000|a100
 
-if [ "$gpu" == 1 ]; then
-  echo "#SBATCH --gres=gpu:1            # Format gpu[[:TYPE]:count]. TYPE is an optional classification for the resource (e.g. a100)">> ./$output
-fi
+# [Array]
+# #SBATCH --array=1-5              # Use \$SLURM_ARRAY_TASK_ID
 
-echo """
-# #SBATCH --account=users         # Uncomment and change if necessary
-""" > ./$output
-
-echo """
 module purge
 #module load [PACKAGE/VERSION] [PACKAGE/VERSION]
-""" >> ./$output
+""" > ./${output}.sbatch
+
+echo "${output}.sbatch created"
+
